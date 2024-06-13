cb44fec25ed47136a362e47b39c7d45cd11cbec5
atharv169 <119473550+atharv169@users.noreply.github.com>
2024-06-13 18:49:55 +0530

    postman use

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -27,13 +27,14 @@ if (
     throw new ApiError(400,"All fields are required")
 }
 
-const existedUser=User.findOne({
+const existedUser=await User.findOne({
     $or:[{username},{email}]
 })
 
 if(existedUser){
     throw new ApiError(409,"USer with email or username already exists")
 }
+console.log(req.files);
 
 const avatarLocalPath=req.files?.avatar[0]?.path;
 const coverImageLocalPath=req.files?.coverImage[0]?.path;
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,5 @@
 import {v2 as cloudinary} from 'cloudinary';
-import fs from "fs";
+import fs, { fdatasync } from "fs";
 
 cloudinary.config({ 
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
@@ -15,9 +15,10 @@ const uploadOnCloudinary=async (localFilePath)=>{
        const response=await  cloudinary.uploader.upload(localFilePath,{
             resource_type:"auto"
         })
-        //file has been uploaded successfull
-        console.log("file is uploaded on cloudinary",response.url)
-        return response;
+       // file has been uploaded successfull
+      // console.log("file is uploaded on cloudinary",response.url)
+      fs.unlinkSync(localFilePath)
+       return response;
     }catch(error){
        fs.unlinkSync(localFilePath)//remove the locally saved temporary file as the upload operation got failed 
     return null;
